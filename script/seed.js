8ce1b47571b8e80a6912c25c96594e4a6e7649e8
Ashutosh Bhalala <bhalala@wisc.edu>
2021-08-12 15:02:00 -0500

    database is seeded and the program is able to run wihtout problems on localhost 8080

--- script/seed.js
+++ script/seed.js
@@ -1,28 +1,106 @@
 'use strict'
 
-const {db, models: {User} } = require('../server/db')
+const {db, models: {User, Project} } = require('../server/db')
+
+const Users = [
+  {
+    username: "bhalala@wisc.edu",
+    password: "Ashutosh44",
+  },
+  {
+    username: "helen@aol.com",
+    password: "helen123",
+  },
+  {
+    username: "billy@aol.com",
+    password: "billy123",
+  },
+  {
+    username: "sam@aol.com",
+    password: "sam123",
+  },
+  {
+    username: "roger@aol.com",
+    password: "roger123",
+  },
+  {
+    username: "angie@aol.com",
+    password: "ANGIE123",
+  },
+  {
+    username: "Gary@aol.com",
+    password: "Gary123",
+  },
+  {
+    username: "William@aol.com",
+    password: "William123",
+  },
+  {
+    username: "Stacey@aol.com",
+    password: "Stacey123",
+  },
+  {
+    username: "Travis@aol.com",
+    password: "Travis123",
+  },
+];
+
+const Projects = [
+  {
+    name: "Grace Shopper",
+    imageURL: "https://toppng.com//public/uploads/preview/shopping-cart-icon-transparent-background-shopping-cart-icon-11553461269twqdfb9nnw.png",
+    role: "Software Developer",
+    description: "Constructed database models for several components of the application such as the user, product, cart, and order models," + 
+    " which resulted in fully connecting database structures that helped organize the backend of the program" +
+    " Using CSS, I was able to style the program’s page to a very user-friendly display, which resulted in a unique and visually appealing webpage.",
+    link: "https://github.com/AmericoneGrizzly/graceshopper-project",
+  },
+  {
+    name: "Junior Phase Final Project",
+    imageURL: "https://www.pngfind.com/pngs/m/55-551472_robot-cartoon-hd-png-download.png",
+    role: "Sole Software Developer",
+    description: "Wired up all the routes and components in order to make this web page fully functional to the point where multiple robots and multiple" + 
+    " projects could coincide and be assigned to one another, as well as all other notable features for the web page.",
+    link: "https://github.com/AshutoshBhalala44/2106-FSA-RM-WEB-FT-JPFP",
+  },
+  {
+    name: "Stackathon",
+    imageURL: "https://spng.pngfind.com/pngs/s/45-453454_png-50-px-inquiry-form-icon-png-transparent.png",
+    role: "Sole Software Developer",
+    description: "Created a web page where a list of personal projects can be displayed with links corresponding to the associated GitHub repos.",
+    link: "https://github.com/AshutoshBhalala44/Stackathon",
+  },
+  
+]
 
 /**
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
 async function seed() {
-  await db.sync({ force: true }) // clears db and matches models to tables
-  console.log('db synced!')
+  try{
+    await db.sync({ force: true }); // clears db and matches models to tables
+    console.log('db synced!');
 
-  // Creating Users
-  const users = await Promise.all([
-    User.create({ username: 'cody', password: '123' }),
-    User.create({ username: 'murphy', password: '123' }),
-  ])
+    // Creating Users
+    await Promise.all(
+      Users.map((user) => {
+        return User.create(user);
+      })
+    );
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
-  return {
-    users: {
-      cody: users[0],
-      murphy: users[1]
-    }
+    await Promise.all(
+      Projects.map((project) => {
+        return Project.create(project);
+      })
+    );
+
+    console.log(`seeded ${Users.length} users`);
+    console.log(`seeded successfully`);
+    console.log(`seeded ${Projects.length} projects`);
+    console.log(`seeded successfully`);
+  } catch (e) {
+    console.log(e);
   }
 }
 
--- server/db/index.js
+++ server/db/index.js
@@ -3,12 +3,16 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const Project = require('./models/Project')
 
 //associations could go here!
+User.hasMany(Project);
+Project.belongsTo(User);
 
 module.exports = {
   db,
   models: {
     User,
+    Project
   },
 }
